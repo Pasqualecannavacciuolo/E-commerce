591cf1d4d648366875f23080dffbc0c50c8c013c
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-06-29 15:12:26 +0200

    Added search filter on courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -11,69 +11,25 @@ export class CoursesComponent implements OnInit {
 
 
   courses: Course[] = []
+  filtered_courses: Course[] = [];
   
-  data: string | undefined;
+  data: string = '';
 
   Users = [{
       "id": 5440,
       "name": "Wanda Lynch",
       "email": "wanda.lynch@example.com"
-    },
-    {
-      "id": 6228,
-      "name": "Katrina Graves",
-      "email": "katrina.graves@example.com"
-    },
-    {
-      "id": 1654,
-      "name": "Louis Daniels",
-      "email": "louis.daniels@example.com"
-    },
-    {
-      "id": 1631,
-      "name": "Gavin Sullivan",
-      "email": "gavin.sullivan@example.com"
-    },
-    {
-      "id": 9880,
-      "name": "June Martinez",
-      "email": "june.martinez@example.com"
-    },
-    {
-      "id": 8634,
-      "name": "Owen Davis",
-      "email": "owen.davis@example.com"
-    },
-    {
-      "id": 3918,
-      "name": "Megan Harrison",
-      "email": "megan.harrison@example.com"
-    },
-    {
-      "id": 3680,
-      "name": "Joel Thompson",
-      "email": "joel.thompson@example.com"
-    },
-    {
-      "id": 2409,
-      "name": "Dora Rose",
-      "email": "dora.rose@example.com"
-    },
-    {
-      "id": 4477,
-      "name": "Candice Neal",
-      "email": "candice.neal@example.com"
-    }
-  ]
+    }];
   
   constructor(private CourseService: CourseService) { }
 
   ngOnInit(): void {
     // Ottengo tutti i corsi dall'API
-    this.CourseService.getAllCourses().subscribe(data => (
-      this.courses = data,
-      console.log(this.courses)
+    this.CourseService.getAllCourses().subscribe(res => (
+      this.courses = res
     ));
   }
+
+  
 
 }
