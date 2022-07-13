5df6598fd3f930531149cbcd653dfb1d8e21067e
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-07-13 15:15:56 +0200

    Migrating from old to new Stripe API checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { Cart } from '../models/Cart';
 import { User } from '../models/User';
 import { CartService } from '../services/cart.service';
@@ -35,7 +34,6 @@ export class CartComponent implements OnInit {
       this.cart = c;
       this.getCartTotal();
     });
-    this.invokeStripe();
   }
 
   // Calcolo il totale del carrello dell'utente
@@ -70,52 +68,27 @@ export class CartComponent implements OnInit {
     );
   }
 
-  makePayment(amount: any) {
-      const paymentHandler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_ULwOwMs5XE4PhRyAYaUvatlD009SOLlwK2',
-      locale: 'auto',
-      token: function (stripeToken: any) {
-        console.log(stripeToken);
-        paymentstripe(stripeToken);
-      },
-    });
-    const paymentstripe = (stripeToken: any) => {
-      
-      this.checkout.makePayment(stripeToken, window.sessionStorage.getItem('id'), amount, this.user?.email).subscribe((data: any) => {
-        console.log(data);
-        //this.checkout.sentTotalAmount(window.sessionStorage.getItem('id'), amount).subscribe();
-        if (data.data === "success") {
-          this.success = true
-        }
-        else {
-          this.failure = true
-        }
-      });
-    };
-    paymentHandler.open({
-      name: 'Il tuo carrello',
-      description: 'Stai comprando tutti i tuoi corsi',
-      amount: amount * 100,
-    });
+  makePayment() {
+    fetch("http://localhost:5000/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: this.cart?.items,
+    }),
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+      return res.json().then(json => Promise.reject(json))
+    })
+    .then(({ url }) => {
+      window.location = url
+    })
+    .catch(e => {
+      console.error(e.error)
+    })
   }
-  invokeStripe() {
-    if (!window.document.getElementById('stripe-script')) {
-      const script = window.document.createElement('script');
-      script.id = 'stripe-script';
-      script.type = 'text/javascript';
-      script.src = 'https://checkout.stripe.com/checkout.js';
-      script.onload = () => {
-        this.paymentHandler = (<any>window).StripeCheckout.configure({
-          key: 'pk_test_ULwOwMs5XE4PhRyAYaUvatlD009SOLlwK2',
-          locale: 'auto',
-          token: function (stripeToken: any) {
-            console.log(stripeToken);
-            alert('Payment has been successfull!');
-          },
-        });
-      };
-      window.document.body.appendChild(script);
-    }
-  }
+  
   
 }
--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Cart } from '../models/Cart';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,10 @@ export class CheckoutService {
 
   constructor(private http: HttpClient) { }
 
-  makePayment(stripeToken: any, id: any, amount: any, email: any): Observable<any> {
-    const url = "http://localhost:5000/checkout/"
+  /*makePayment(stripeToken: any, id: any, amount: number, email: string): Observable<any> {
+    const url = "http://localhost:5000/checkout";
     return this.http.post<any>(url,{token:stripeToken, cartID: id, cartAmount: amount, userEmail: email})
-  }
+  }*/
 
   /*sentTotalAmount(id: any, amount: any): Observable<any> {
     const url = `http://localhost:5000/cart_amount/`;
