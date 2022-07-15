b587172881aa3b82cf683712f35827e575c998e3
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-07-15 11:23:31 +0200

    Styling dashboard component

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  actions = ['Crea Admin', 'Crea Prodotto', "Modifica prodotto", "Cancella Prodotto"]
 
+  constructor() { }
+  
   ngOnInit(): void {
   }
+
 
 }
